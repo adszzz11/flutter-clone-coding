824e94fb3af7c82b8cdaecd15d722ef4de3162f0
LeeTangle <adszzz11@naver.com>
2022-02-24 22:40:46 +0900

    main / create Story, padding

--- flutter_clone_insta/lib/src/pages/home.dart
+++ flutter_clone_insta/lib/src/pages/home.dart
@@ -40,14 +40,52 @@ class Home extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: List.generate(
-            100,
-            (index) => AvatarWidget(thumbPath: 'https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg', type: AvatarType.TYPE1,)),
+        children: [
+          const SizedBox(
+            width: 15,
+          ),
+          _myStory(),
+          const SizedBox(
+            width: 5,
+          ),
+          ...List.generate(
+              100,
+              (index) => AvatarWidget(
+                    thumbPath:
+                        'https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg',
+                    type: AvatarType.TYPE1,
+                  )),
+        ],
       ),
     );
   }
 
   Widget _postList() {
     return Container();
+  }
+
+  Widget _myStory() {
+    return Stack(
+      children: [
+        AvatarWidget(
+          thumbPath:
+              'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+          type: AvatarType.TYPE2,
+          size: 70,
+        ),
+        Positioned(
+            right: 5,
+            bottom: 0,
+            child: Container(
+              width: 25,
+              height: 25,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue,
+                  border: Border.all(color: Colors.white, width: 2)),
+              child: const Center(child: Text("+", style: TextStyle(fontSize: 20,color: Colors.white,height: 1.1),),),
+            ))
+      ],
+    );
   }
 }
