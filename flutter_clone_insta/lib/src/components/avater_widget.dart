f001c7895dc6388dc42c602c53d4d06b7103df70
LeeTangle <adszzz11@naver.com>
2022-02-24 22:41:11 +0900

    main / create Story

--- flutter_clone_insta/lib/src/components/avater_widget.dart
+++ flutter_clone_insta/lib/src/components/avater_widget.dart
@@ -15,7 +15,7 @@ class AvatarWidget extends StatelessWidget {
       required this.type,
       this.hasStory,
       this.nickname,
-      this.size,
+      this.size = 65,
       Key? key})
       : super(key: key);
 
@@ -26,7 +26,7 @@ class AvatarWidget extends StatelessWidget {
         return type1Widget();
         break;
       case AvatarType.TYPE2:
-        // return type2Widget();
+        return type2Widget();
         break;
       case AvatarType.TYPE3:
         // return type3Widget();
@@ -43,6 +43,7 @@ class AvatarWidget extends StatelessWidget {
       width: 65,
       height: 65,
       margin: const EdgeInsets.symmetric(horizontal: 5),
+      padding: const EdgeInsets.all(2),
       decoration: const BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.topRight,
@@ -50,11 +51,20 @@ class AvatarWidget extends StatelessWidget {
               colors: [Colors.purple, Colors.orange]),
           shape: BoxShape.circle,
           color: Colors.grey),
+      child: type2Widget(),
+    );
+  }
+
+  Widget type2Widget() {
+    return Container(
+      padding: const EdgeInsets.all(2),
+      decoration:
+          const BoxDecoration(color: Colors.white, shape: BoxShape.circle),
       child: ClipRRect(
-        borderRadius: BorderRadius.circular(65),
-        child: Container(
-          width: 65,
-          height: 65,
+        borderRadius: BorderRadius.circular(size!),
+        child: SizedBox(
+          width: size,
+          height: size,
           child: CachedNetworkImage(
             imageUrl: thumbPath,
             fit: BoxFit.cover,
