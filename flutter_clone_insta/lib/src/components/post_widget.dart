375ba2ca53a55493c958d57fd3703a3ca31b5612
sm <adszzz11@naver.com>
2022-02-28 15:49:40 +0900

    main / modified post_widget.dart - Home complete

--- flutter_clone_insta/lib/src/components/post_widget.dart
+++ flutter_clone_insta/lib/src/components/post_widget.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_clone_insta/src/components/avater_widget.dart';
+import 'package:flutter_clone_insta/src/components/image_data.dart';
 
 class PostWidget extends StatelessWidget {
   const PostWidget({Key? key}) : super(key: key);
@@ -7,25 +10,163 @@ class PostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
+      // height: 150,
       margin: EdgeInsets.only(top: 20),
-      color: Colors.red,
-      child: Column(children: [
-        _header(),
-        // _image(),
-        // _infoCount(),
-        // _infoDescription(),
-        // _replyTextBtn(),
-        // _dateAgo(),
-      ],),
+      // color: Colors.red,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _header(),
+          const SizedBox(
+            height: 15,
+          ),
+          _image(),
+          const SizedBox(
+            height: 15,
+          ),
+          _infoCount(),
+          const SizedBox(
+            height: 5,
+          ),
+          _infoDescription(),
+          const SizedBox(
+            height: 5,
+          ),
+          _replyTextBtn(),
+          const SizedBox(
+            height: 5,
+          ),
+          _dateAgo(),
+        ],
+      ),
     );
   }
 
   Widget _header() {
-    return Row(
-      children: [
-        AvatarWidget(thumbPath: '', type: AvatarType.TYPE3),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AvatarWidget(
+            thumbPath:
+                'https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg',
+            type: AvatarType.TYPE3,
+            nickname: '상민',
+            size: 40,
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ImageData(
+                IconsPath.postMoreIcon,
+                width: 30,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _image() {
+    return CachedNetworkImage(
+        imageUrl:
+            'https://i1.sndcdn.com/artworks-000666741013-2j4tel-t500x500.jpg');
+  }
+
+  Widget _infoCount() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              ImageData(
+                IconsPath.likeOffIcon,
+                width: 65,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              ImageData(
+                IconsPath.replyIcon,
+                width: 60,
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              ImageData(
+                IconsPath.directMessage,
+                width: 55,
+              ),
+            ],
+          ),
+          ImageData(
+            IconsPath.bookMarkOffIcon,
+            width: 50,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _infoDescription() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '좋아요, 150개',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          ExpandableText(
+            '컨텐츠 1 입니다.\n컨텐츠 1 입니다.\n컨텐츠 1 입니다.\n컨텐츠 1 입니다.\n',
+            prefixText: '상민',
+            onPrefixTap: () {
+              print('상민 프로필 페이지 이동');
+            },
+            prefixStyle: TextStyle(fontWeight: FontWeight.bold),
+            expandText: '더보기',
+            collapseText: '접기',
+            maxLines: 3,
+            expandOnTextTap: true,
+            collapseOnTextTap: true,
+            linkColor: Colors.grey,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _replyTextBtn() {
+    return GestureDetector(
+      onTap: () {},
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        child: Text(
+          '댓글 199개 모두 보기',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 13,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _dateAgo() {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        '1일 전',
+        style: TextStyle(color: Colors.grey, fontSize: 11),
+      ),
     );
   }
 }
