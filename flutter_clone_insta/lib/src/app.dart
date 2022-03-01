92c80da51715fffce4013031144561f57fb06192
sm <adszzz11@naver.com>
2022-03-01 18:53:50 +0900

    main / modified app.dart - fix Get Bug

--- flutter_clone_insta/lib/src/app.dart
+++ flutter_clone_insta/lib/src/app.dart
@@ -12,53 +12,59 @@ class App extends GetView<BottomNavController> {
   Widget build(BuildContext context) {
     return WillPopScope(
       child: Obx(
-          () =>Scaffold(
-          body: IndexedStack(
-            index: controller.pageIndex.value,
-            children: [
-              const Home(),
-              const Search(),
-              Container(child: Center(child: Text('upload')),),
-              Container(child: Center(child: Text('activity')),),
-              Container(child: Center(child: Text('mypage')),),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            currentIndex: controller.pageIndex.value,
-            elevation: 0,
-            onTap: controller.changeBottomNav,
-            items: [
-              BottomNavigationBarItem(
-                  icon: ImageData(IconsPath.homeOff),
-                  activeIcon: ImageData(IconsPath.homeOn),
-                  label: 'home'),
-              BottomNavigationBarItem(
-                  icon: ImageData(IconsPath.searchOff),
-                  activeIcon: ImageData(IconsPath.searchOn),
-                  label: 'search'),
-              BottomNavigationBarItem(
-                  icon: ImageData(IconsPath.uploadIcon),
-                  // activeIcon: ImageData(IconsPath.homeOn),
-                  label: 'upload'),
-              BottomNavigationBarItem(
-                  icon: ImageData(IconsPath.activeOff),
-                  activeIcon: ImageData(IconsPath.activeOn),
-                  label: 'active'),
-              BottomNavigationBarItem(
-                  icon: Container(
-                    width: 30,
-                    height: 30,
-                    decoration:
-                        BoxDecoration(shape: BoxShape.circle, color: Colors.grey),
-                  ),
-                  activeIcon: ImageData(IconsPath.homeOn),
-                  label: 'home'),
-            ],
-          ),
-        ),
+            () =>
+            Scaffold(
+              body: IndexedStack(
+                index: controller.pageIndex.value,
+                children: [
+                  const Home(),
+                  Navigator(key: controller.searchPageNavigationKey,
+                    onGenerateRoute: (routeSetting) {
+                      return MaterialPageRoute(builder: (context) => Search());
+                    },),
+                  // const Search(),
+                  Container(child: Center(child: Text('upload')),),
+                  Container(child: Center(child: Text('activity')),),
+                  Container(child: Center(child: Text('mypage')),),
+                ],
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                type: BottomNavigationBarType.fixed,
+                showSelectedLabels: false,
+                showUnselectedLabels: false,
+                currentIndex: controller.pageIndex.value,
+                elevation: 0,
+                onTap: controller.changeBottomNav,
+                items: [
+                  BottomNavigationBarItem(
+                      icon: ImageData(IconsPath.homeOff),
+                      activeIcon: ImageData(IconsPath.homeOn),
+                      label: 'home'),
+                  BottomNavigationBarItem(
+                      icon: ImageData(IconsPath.searchOff),
+                      activeIcon: ImageData(IconsPath.searchOn),
+                      label: 'search'),
+                  BottomNavigationBarItem(
+                      icon: ImageData(IconsPath.uploadIcon),
+                      // activeIcon: ImageData(IconsPath.homeOn),
+                      label: 'upload'),
+                  BottomNavigationBarItem(
+                      icon: ImageData(IconsPath.activeOff),
+                      activeIcon: ImageData(IconsPath.activeOn),
+                      label: 'active'),
+                  BottomNavigationBarItem(
+                      icon: Container(
+                        width: 30,
+                        height: 30,
+                        decoration:
+                        BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey),
+                      ),
+                      activeIcon: ImageData(IconsPath.homeOn),
+                      label: 'home'),
+                ],
+              ),
+            ),
       ),
       onWillPop: controller.willPopAction,
     );
