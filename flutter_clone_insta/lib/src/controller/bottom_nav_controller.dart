01c27952732076bb67c3fe6a2b0a38e0e2950cd9
sm <adszzz11@naver.com>
2022-03-01 18:53:29 +0900

    main / modofied bottom_nav_controller.dart - fix Get Bug

--- flutter_clone_insta/lib/src/controller/bottom_nav_controller.dart
+++ flutter_clone_insta/lib/src/controller/bottom_nav_controller.dart
@@ -9,7 +9,10 @@ import '../pages/upload.dart';
 enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
 
 class BottomNavController extends GetxController {
+  static BottomNavController get to => Get.find();
   RxInt pageIndex = 0.obs;
+  GlobalKey<NavigatorState> searchPageNavigationKey =
+      GlobalKey<NavigatorState>();
   List<int> bottomHistory = [0];
 
   void changeBottomNav(int value, {bool hasGesture = true}) {
@@ -85,6 +88,12 @@ class BottomNavController extends GetxController {
       print('exit');
       return true;
     } else {
+      var page = PageName.values[bottomHistory.last];
+      if (page == PageName.SEARCH) {
+        var value = await searchPageNavigationKey.currentState!.maybePop();
+        if (value) return false;
+      }
+
       bottomHistory.removeLast();
       print(bottomHistory);
       changeBottomNav(bottomHistory.last, hasGesture: false);
