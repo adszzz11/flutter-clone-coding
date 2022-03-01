3b7eb2c7e00b8221d7a77cc09fcb51a0bf84e3df
sm <adszzz11@naver.com>
2022-03-01 19:58:26 +0900

    main / upload.dart - init setting

--- flutter_clone_insta/lib/src/pages/upload.dart
+++ flutter_clone_insta/lib/src/pages/upload.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_insta/src/components/image_data.dart';
+import 'package:get/get.dart';
 
 class Upload extends StatelessWidget {
   const Upload({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(IconsPath.closeImage),
+          ),
+        ),
+        title: const Text(
+          'New Post',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _imagePreview(),
+            _header(),
+            // _imageSelectList(),
+          ],
+        ),
+      ),
     );
+  }
+
+  Widget _imagePreview() {
+    return Container(
+      width: Get.width,
+      height: Get.height,
+      color: Colors.grey,
+    );
+  }
+
+  Widget _header() {
+    return Container();
   }
 }
