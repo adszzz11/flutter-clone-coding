38da01acd0f98a13730985e8450332306b624d4a
sm <adszzz11@naver.com>
2022-03-04 21:57:20 +0900

    main / modified upload.dart - page complete

--- flutter_clone_insta/lib/src/pages/upload.dart
+++ flutter_clone_insta/lib/src/pages/upload.dart
@@ -42,7 +42,7 @@ class Upload extends StatelessWidget {
           children: [
             _imagePreview(),
             _header(),
-            // _imageSelectList(),
+            _imageSelectList(),
           ],
         ),
       ),
@@ -52,12 +52,79 @@ class Upload extends StatelessWidget {
   Widget _imagePreview() {
     return Container(
       width: Get.width,
-      height: Get.height,
+      height: Get.width,
       color: Colors.grey,
     );
   }
 
   Widget _header() {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  '갤러리',
+                  style: TextStyle(color: Colors.black, fontSize: 18),
+                ),
+                Icon(Icons.arrow_drop_down),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                    color: const Color(0xff808080),
+                    borderRadius: BorderRadius.circular(30)),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    Text(
+                      '여러 항목 선택',
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle, color: Color(0xff808080)),
+                child: ImageData(IconsPath.cameraIcon),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          mainAxisSpacing: 1,
+          crossAxisSpacing: 1,
+          childAspectRatio: 1),
+      itemCount: 100,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          color: Colors.red,
+        );
+      },
+    );
   }
 }
