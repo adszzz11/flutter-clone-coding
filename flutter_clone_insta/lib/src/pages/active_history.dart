393742dd5358e48c34bfad64219df5f045697e42
sm <adszzz11@naver.com>
2022-03-07 22:15:17 +0900

    main / create active_history.dart - active page complete

--- flutter_clone_insta/lib/src/pages/active_history.dart
+++ flutter_clone_insta/lib/src/pages/active_history.dart
@@ -1,12 +1,129 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_insta/src/components/avater_widget.dart';
 
 class ActiveHistory extends StatelessWidget {
   const ActiveHistory({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          '활동',
+          style: TextStyle(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _newRecentlyActiveView(),
+            _newRecentlyThisWeekView(),
+            _newRecentlyThisMonthView(),
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget _newRecentlyActiveView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '오늘',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne()
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyThisWeekView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '이번 주',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+        ],
+      ),
+    );
+  }
+
+  Widget _newRecentlyThisMonthView() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            '이번 달',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          _activeItemOne(),
+          _activeItemOne(),
+          _activeItemOne(),
+        ],
+      ),
+    );
+  }
+
+  Widget _activeItemOne() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: [
+          AvatarWidget(
+            thumbPath:
+                'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+            type: AvatarType.TYPE2,
+            size: 40,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Expanded(
+            child: Text.rich(
+              TextSpan(
+                text: '상민, 수진, 동영, 선제',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                children: [
+                  TextSpan(
+                      text: ' 님이 회원님의 게시물을 좋아합니다.',
+                      style: TextStyle(fontWeight: FontWeight.normal)),
+                  TextSpan(
+                      text: ' 5분 전',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: 13,
+                          color: Colors.black54)),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
