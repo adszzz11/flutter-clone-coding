54ec0911d526c9cdbc7e24c1713f489c83348341
sm <adszzz11@naver.com>
2022-03-10 21:34:22 +0900

    main / modified mypage.dart - _information()

--- flutter_clone_insta/lib/src/pages/mypage.dart
+++ flutter_clone_insta/lib/src/pages/mypage.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clone_insta/src/components/avater_widget.dart';
+import 'package:flutter_clone_insta/src/components/image_data.dart';
 
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        title: const Text(
+          '상민',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: ImageData(
+              IconsPath.uploadIcon,
+              width: 50,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: ImageData(
+                IconsPath.menuIcon,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _infomation(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _infomation() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            children: [
+              AvatarWidget(
+                thumbPath:
+                    'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+                type: AvatarType.TYPE3,
+                size: 80,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(child: _statisticsOne('Post', 15)),
+                    Expanded(child: _statisticsOne('Followers', 15)),
+                    Expanded(child: _statisticsOne('Following', 15)),
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            '안녕, 내 이름은 상민 !',
+            style: TextStyle(fontSize: 13, color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _statisticsOne(String title, int value) {
+    return Column(
+      children: [
+        Text(
+          value.toString(),
+          style: TextStyle(
+              fontSize: 20, color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          title,
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        ),
+      ],
+    );
   }
 }
